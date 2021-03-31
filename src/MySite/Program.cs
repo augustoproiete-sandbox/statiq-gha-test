d92779860c0d6f708bcf5a88aa466abc3bf17578
C. Augusto Proiete <augusto@proiete.com>
2021-03-31 20:46:09 -0300

    Statiq starter

---
+++ src/MySite/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace MySite
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync();
+        }
+    }
+}
